898bff7e2a45ab54ae2bddcfdf0fa3b36a2ce03a
nguy4892@umn.edu <nguy4892@umn.edu>
2026-08-03 16:42:22 -0500

    automasters update

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const overlay = document.getElementById("overlay");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    overlay.classList.remove("active");
+});
